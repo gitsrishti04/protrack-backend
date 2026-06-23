c5a67c77e2fd542bad6d30dfaf427d5156c9809f
Srishti khandelwal <srishti@Mac.lan>
2026-06-23 12:53:49 +0530

    migration of database

--- app/database.py
+++ app/database.py
@@ -1,12 +1,30 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://srishti@localhost/protrack_db"
-engine =create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine)
+load_dotenv()
 
-Base =declarative_base()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
